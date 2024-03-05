7259c90d262c0160b448defc0b99b6c663192bf6
ethancreates <ethancreatesfiles@gmail.com>
2024-03-06 02:12:29 +0800

    removed typo x on the mainbutton component

--- src/components/ui/dotted-button.tsx
+++ src/components/ui/dotted-button.tsx
@@ -9,7 +9,6 @@ type MainButtonProps = {
 const MainButton = ({ children, className }: MainButtonProps) => {
   return (
     <button
-      x
       className={cn(
         className,
         "rounded-md border border-white bg-slate-900 px-6 font-light uppercase h-12  text-white transition-all duration-300  hover:translate-y-[10px] hover:rounded-2xl  active:translate-x-[0px] active:translate-y-[0px] active:rounded-full active:shadow-none"
